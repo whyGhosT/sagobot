db0f015fc70c2a58536fc24ae546fc46390b276d
kaan~ <81658277+whyGhosT@users.noreply.github.com>
2022-06-13 21:14:28 +0300

    base

--- sagodc.py
+++ sagodc.py
@@ -9,15 +9,22 @@ sago = commands.Bot(command_prefix="mf! ")
 
 @sago.event
 async def on_ready():
-    await sago.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Kağıt Kesikleri"))
-    print("waow")
+    await sago.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="mf! yardım | Kağıt Kesikleri OUT!"))
+    print("Bot başarılı şekilde çalıştırıldı.")
+    
+@sago.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+    embed=discord.Embed(title=f"Yavaş ol. {:.2f}s'.format(retry_after) sonra tekrar dene..")
+    embed.set_author(name="SagoBot", url="https://whyghost.github.io")
+    embed.set_footer(text="by wg-")
+    await ctx.send(embed=embed)
 
-@sago.command(aliases=['versiyon']
+@sago.command(aliases=['versiyon'])
 @commands.cooldown(1,5,commands.BucketType.user)
 async def version(ctx):
-    embed=discord.Embed(title="SagoBot")
-    embed.set_author(name="wg-", url="https://whyghost.github.io")
-    embed.add_field(name="Version:", value="0.1", inline=False)
+    embed=discord.Embed(title="Version : 0.1")
+    embed.set_author(name="SagoBot", url="https://whyghost.github.io")
     embed.set_footer(text="by wg-")
     await ctx.send(embed=embed)
 
