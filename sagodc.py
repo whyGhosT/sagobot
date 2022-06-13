31ae766ddc557ad5c35393e5f2e58f192d60728d
kaan~ <81658277+whyGhosT@users.noreply.github.com>
2022-06-13 18:53:45 +0300

    version

--- sagodc.py
+++ sagodc.py
@@ -15,4 +15,8 @@ async def on_ready():
 @sago.command(aliases=['versiyon']
 @commands.cooldown(1,5,commands.BucketType.user)
 async def version(ctx):
-    await ctx.send("dur la")
+    embed=discord.Embed(title="SagoBot")
+    embed.set_author(name="wg-", url="https://whyghost.github.io")
+    embed.add_field(name="Version:", value="0.1", inline=False)
+    embed.set_footer(text="by wg-")
+    await ctx.send(embed=embed)
