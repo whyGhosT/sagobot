5444baf8a123872e267be65c080577ab00a8fd59
kaan~ <81658277+whyGhosT@users.noreply.github.com>
2022-06-14 11:36:22 +0300

    sagolandın komutu eklendi

--- sagodc.py
+++ sagodc.py
@@ -2,6 +2,7 @@ import discord
 import os
 import random
 from discord.ext import commands
+from simages import rastgelesago
 
 sagotoken = ""
 
@@ -27,16 +28,43 @@ async def version(ctx):
     embed.set_author(name="SagoBot", url="https://whyghost.github.io")
     embed.set_footer(text="by wg-")
     await ctx.send(embed=embed)
-
+    
 @sago.command(aliases=['help'])
 @commands.cooldown(1,5,commands.BucketType.user)
-async def yardım(ctx):
-    embed=discord.Embed()
-    embed.set_author(name="Yardım", url="https://whyghost.github.io", icon_url="https://w0.peakpx.com/wallpaper/531/278/HD-wallpaper-sagopa-kajmer-turkce-rap-thumbnail.jpg")
-    embed.add_field(name="Eğlence Komutları İçin", value="mf! yardım eğlence", inline=True)
-    embed.add_field(name="Müzik Komutları İçin", value="mf! yardım müzik", inline=True)
-    embed.add_field(name="Diğer Komutlar İçin", value="mf! yardım diğer", inline=True)
-    embed.set_footer(text="by wg-")
-    await ctx.send(embed=embed)
+async def yardım(ctx, *args):
+    if "eğlence" in args:
+        embed=discord.Embed()
+        embed.set_author(name="Eğlence Komutları", url="https://whyghost.github.io", icon_url="https://w0.peakpx.com/wallpaper/531/278/HD-wallpaper-sagopa-kajmer-turkce-rap-thumbnail.jpg")
+        embed.add_field(name="mf! sagola", value="Sagolan!", inline=True)
+        embed.set_footer(text="by wg-")
+        await ctx.send(embed=embed)
+    else:
+        embed=discord.Embed()
+        embed.set_author(name="Yardım", url="https://whyghost.github.io", icon_url="https://w0.peakpx.com/wallpaper/531/278/HD-wallpaper-sagopa-kajmer-turkce-rap-thumbnail.jpg")
+        embed.add_field(name="Eğlence Komutları İçin", value="mf! yardım eğlence", inline=True)
+        embed.add_field(name="Müzik Komutları İçin", value="mf! yardım müzik", inline=True)
+        embed.add_field(name="Diğer Komutlar İçin", value="mf! yardım diğer", inline=True)
+        embed.set_footer(text="by wg-")
+        await ctx.send(embed=embed)
+
+@sago.command()
+async def sagola(ctx):
+@commands.cooldown(1,5,commands.BucketType.user)
+    yuh = random.randint(1,2)
+    if yuh == 1:
+        abo = random.randint(75,217)
+        embed=discord.Embed()
+        embed.set_author(name="Sagolandın!", url="https://whyghost.github.io", icon_url="https://w0.peakpx.com/wallpaper/531/278/HD-wallpaper-sagopa-kajmer-turkce-rap-thumbnail.jpg")
+        embed.set_footer(text="by wg-")
+        embed.set_image(url=f"https://galeri.bidibidi.com/albumler/galerim/11920/616/normal_Sagopa_({abo}).jpg")
+        await ctx.send(embed=embed)
+    else:
+        ah = rastgelesago.copy()
+        pu = random.choice(ah)
+        embed=discord.Embed()
+        embed.set_author(name="Sagolandın!", url="https://whyghost.github.io", icon_url="https://w0.peakpx.com/wallpaper/531/278/HD-wallpaper-sagopa-kajmer-turkce-rap-thumbnail.jpg")
+        embed.set_footer(text="by wg-")
+        embed.set_image(url=pu)
+        await ctx.send(embed=embed)
 
 sago.run(sagotoken)
---
+++ simages.py
@@ -0,0 +1,15 @@
+rastgelesago = ['https://i.pinimg.com/736x/ed/d5/03/edd5032117dac523c4275e0c997d8545.jpg',
+				'https://i.pinimg.com/736x/b0/4a/a1/b04aa18b0575a07278c40da695b4f149.jpg',
+				'https://pbs.twimg.com/media/EQ10gKBWkAA1gj4.jpg',
+				'https://pbs.twimg.com/media/DbuX98yWkAEceyM.jpg',
+				'https://i.milliyet.com.tr/MolatikGaleri/2019/06/10/fft373_mf32867512.Jpeg',
+				'https://canerkorkmazli.com.tr/wp-content/uploads/sagopa-kajmer-bursa-konseri-1.jpg',
+				'https://www.konya.bel.tr/bldfoto/11/klt/sagopa_6.jpg'
+				'https://maksatbilgi.com/wp-content/uploads/2019/04/Sagopa-Kajmer-2019-Foto-Galeri-17.jpg'
+				'https://c.tenor.com/dGzeJ6myqs4AAAAC/snake-dance.gif'
+				'https://s8.gifyu.com/images/ezgif-2-6bfceba1921.gif'
+				'https://j.gifs.com/vVMVzq.gif'
+				'https://j.gifs.com/KeJaV8.gif'
+				'https://pbs.twimg.com/media/D0FsBpZXgAAlNkc.jpg'
+				'https://c.tenor.com/Bei7bqjm-qwAAAAd/sagopa-sagopa-dans.gif']
+				
